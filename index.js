2e28f0d6b5c6cec211b9a427ff2353b12a9e4d49
julianapeace <chancecordelia@gmail.com>
2022-03-10 00:32:36 -0600

    discord bot will dm you

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
-const config = require('./config.json')
 const ethers = require('ethers')
+const config = require('./config.json')
 const ZKSTAKE = require('./zkStake.json')
 const client = new Discord.Client({
   intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'],
@@ -30,26 +30,42 @@ const checkWhitelist = async (message) => {
   return r
 }
 client.on('messageCreate', async function (message) {
-  console.log(message)
   if (message.author.bot) return
   if (!message.content.startsWith(prefix)) return
+
+  client.channels.cache.get('934173497624768512').send('Hello here!')
+  // console.log(client.channels.cache)
+
   //console.log(client.users)
   //console.log(client.channels)
+
   const commandBody = message.content.slice(prefix.length)
   const args = commandBody.split(' ')
 
   const command = args.shift().toLowerCase()
   console.log(args)
+
   if (command === 'ping') {
     const timeTaken = Date.now() - message.createdTimestamp
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`)
+
+    let role = message.guild.roles.cache.find(r => r.name === "Validated User");
+    let member = message.member;
+    member.roles.add(role).catch(console.error);
+
   } else if (command === 'sum') {
     const numArgs = args.map((x) => parseFloat(x))
     const sum = numArgs.reduce((counter, x) => (counter += x))
     message.reply(`The sum of all the arguments you provided is ${sum}!`)
+  } else if (command === 'dm') {
+    // let messageContent = message.content.replace("!dm", "")
+    message.member.send('Hello! I am ZK-NFT-BOT 🤖 To verify yourself, run "!verify insert-your-proof-here"')
   } else if (command === 'verify') {
+    console.log('DM!!!')
+
     let valid = await checkWhitelist(args[0])
     console.log(valid)
+
     if (valid) {
       message.author.send('GM you are being verified')
     }
